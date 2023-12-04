77c325b05716067b2306681f50f308309570858b
vuongle.dev <vuongle.dev@gmail.com>
2023-12-04 10:18:58 +0700

    - Fix Figma homework image url

--- src/Session3/FigmaApp/index.tsx
+++ src/Session3/FigmaApp/index.tsx
@@ -23,20 +23,20 @@ export default function FigmaApp({}: Props) {
         setNextPage={setNextPage}
         slide={[
           {
-            images: "Day03/FigmaApp/banner1.svg",
+            images: `${process.env.PUBLIC_URL}/Day03/FigmaApp/banner1.svg`,
             title: "Choose a Favourite Food",
             description:
               "When you oder Eat Steet, we'll hook you up with exclusive coupon, specials and rewards",
           },
           {
-            images: "Day03/FigmaApp/banner2.svg",
-            backgroundmage: "Day03/FigmaApp/background2.svg",
+            images: `${process.env.PUBLIC_URL}/Day03/FigmaApp/banner2.svg`,
+            backgroundmage: `${process.env.PUBLIC_URL}/Day03/FigmaApp/background2.svg`,
             title: "Hot Delivery to Home",
             description:
               "We make food ordering fasr, simple and free-no matter if you order online or cash",
           },
           {
-            images: "Day03/FigmaApp/banner3.svg",
+            images: `${process.env.PUBLIC_URL}/Day03/FigmaApp/banner3.svg`,
             title: "Receive the Great Food",
             description:
               "You’ll receive the great food within a hour. And get free delivery credits for every order.",
